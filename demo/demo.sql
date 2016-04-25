ad190ec8ae96db5fd1b1f6d5af47736d9202243a
Eclipse <libeclipse@gmail.com>
2016-04-25 19:48:29 +0000

    Update demo.sql

--- demo/demo.sql
+++ demo/demo.sql
@@ -4,8 +4,8 @@ USE demo;
 
 CREATE TABLE users (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL);
-)
+    username VARCHAR(30) NOT NULL
+    );
 
 INSERT INTO users
     (username)
